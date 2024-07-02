1c4f1c832a2748521159720d8b6fb8aa0ee31ca2
NataliaTopornitska <natalia.topornitska3@gmail.com>
2024-07-02 20:19:06 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,18 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", function() {
+    const thumbs = document.getElementById("thumbs");
+    const largeImg = document.getElementById("largeImg");
+    thumbs.addEventListener("click", function(e) {
+        e.preventDefault();
+        let target = e.target;
+        while(target !== thumbs){
+            if (target.tagName === "A") {
+                largeImg.src = target.href;
+                break;
+            }
+            target = target.parentNode;
+        }
+    });
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
---
+++ index.fc8b6e11.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.getElementById("thumbs"),t=document.getElementById("largeImg");e.addEventListener("click",function(n){n.preventDefault();for(var a=n.target;a!==e;){if("A"===a.tagName){t.src=a.href;break}a=a.parentNode}})});
+//# sourceMappingURL=index.fc8b6e11.js.map
